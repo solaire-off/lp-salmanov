6f2cdcde7ca7dbc38dd552a061bdb24d9b43dbda
solaire-off <victor374288@mail.ru>
2018-01-29 19:56:59 +0300

    Fix form color, add simple adaptive layout, add anhor, change video

--- app/js/common.js
+++ app/js/common.js
@@ -1,38 +1,49 @@
 $(function() {
 
-
-$('.slider').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  arrows: false,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
+    $('.slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        arrows: false,
+        slidesToShow: 5,
         slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    var $root = $('html, body');
+
+    $('.anchor-link').click(function () {
+        $(this).blur();
+        $url = '#'+$(this).data('anchor')
+        $root.animate({
+            scrollTop: $($url).offset().top
+        }, 700);
+
+        return false;
+    });
 
 });
